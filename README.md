a62210412de9e52fa4eaa7bcca889b759d5c4ea6
rezakho <rezakho@gmail.com>
2014-06-25 19:37:39 +0430

    QueryBuilder test added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Qlake PHP Framework
 
+[![Build Status](https://travis-ci.org/rezakho/qlake.svg)](https://travis-ci.org/rezakho/qlake)
+
 A modern PHP 5.4 framework, too fast and too extensible core!
 
 The beta release coming soon!
--- tests/Database/DatebaseQueryBuilderTest.php
+++ tests/Database/DatebaseQueryBuilderTest.php
@@ -11,7 +11,7 @@ class DatabaseQueryBuilderTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('SELECT * FROM `table`', $sql);
 	}
 
-	public function testColumnsSelect()
+	public function testSimpleColumnsSelect()
 	{
 		$query = $this->getQuery();
 
@@ -19,6 +19,24 @@ class DatabaseQueryBuilderTest extends PHPUnit_Framework_TestCase
 
 		$this->assertEquals('SELECT `id`, `name` FROM `table`', $sql);
 	}
+
+	public function testAliasedColumnsSelect()
+	{
+		$query = $this->getQuery();
+
+		$sql = $query->select('family AS f')->from('table')->toSql();
+
+		$this->assertEquals('SELECT family AS f FROM `table`', $sql);
+	}
+
+	/*public function testAggregatedColumnsSelect()
+	{
+		$query = $this->getQuery();
+
+		$sql = $query->select('COUNT(*)')->from('table')->toSql();
+
+		$this->assertEquals('SELECT COUNT(*) FROM `table`', $sql);
+	}*/
 
 	public function getQuery()
 	{
