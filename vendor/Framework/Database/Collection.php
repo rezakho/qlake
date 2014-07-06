49d13c635f848138d40a061abe16ac8215d42924
rezakho <rezakho@gmail.com>
2014-07-06 20:04:49 +0430

    QB

---
+++ vendor/Framework/Database/Collection.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Framework\Database;
+
+use PDOStatement;
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+
+class Collection implements /*ArrayableInterface,*/ ArrayAccess, Countable, IteratorAggregate, /*JsonableInterface*/
+{
+	protected $statement;
+
+	protected $items = [];
+
+	public function __construct(PDOStatement $statement)
+	{
+		$this->statement = $statement;	
+	}
+
+	public function offsetSet($offset, $value)
+	{
+		if (is_null($offset))
+		{
+			$this->items[] = $value;
+		}
+		else
+		{
+			$this->items[$offset] = $value;
+		}
+	}
+
+	public function offsetExists($offset)
+	{
+		return isset($this->items[$offset]);
+	}
+
+	public function offsetUnset($offset)
+	{
+		unset($this->items[$offset]);
+	}
+	
+	public function offsetGet($offset)
+	{
+		return $this->items[$offset] ?: null;
+	}
+
+	public function count()
+	{
+		return count($this->items);
+	}
+
+	public function getIterator()
+	{
+		return new ArrayIterator($this->items);
+	}
+}
